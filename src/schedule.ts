f7dd35eeb9112f73c3987c6bbf75dc3a63662402
Adam <adam@haglund.dev>
2024-06-07 22:41:58 +0200

    add stream URLs

--- src/schedule.ts
+++ src/schedule.ts
@@ -13,14 +13,17 @@ export const events = [
   {
     name: "Summer Game Fest",
     dateTime: new Date("2024-06-07 23:00Z+2"),
+    url: "https://www.youtube.com/watch?v=pZzia5NrMuU",
   },
   {
     name: "Day of the Devs",
     dateTime: new Date("2024-06-08 01:00Z+2"),
+    url: "https://www.youtube.com/watch?v=pZzia5NrMuU",
   },
   {
     name: "Devolver's annual games showcase",
     dateTime: new Date("2024-06-08 02:00Z+2"),
+    url: "https://www.youtube.com/watch?v=pZzia5NrMuU",
   },
   {
     name: "Future of Play Direct",
@@ -41,17 +44,21 @@ export const events = [
   {
     name: "Future Games Show",
     dateTime: new Date("2024-06-08 21:00Z+2"),
+    url: "https://www.youtube.com/watch?v=1jXmyz452hs",
   },
   {
     name: "Xbox Games Showcase",
     dateTime: new Date("2024-06-09 19:00Z+2"),
+    url: "https://www.youtube.com/watch?v=8LZM-mxaPlQ",
   },
   {
     name: "PC Gaming Show",
     dateTime: new Date("2024-06-09 22:00Z+2"),
+    url: "https://www.youtube.com/watch?v=af4QRDcl3r8",
   },
   {
     name: "Ubisoft Backwards",
     dateTime: new Date("2024-06-10 21:00Z+2"),
+    url: "https://www.youtube.com/watch?v=0QNRXquzTts",
   },
 ] satisfies Event[]
